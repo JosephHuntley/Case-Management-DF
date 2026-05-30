bdbc0c213d2e8ee96dd7110a25a6033903a9b470
Joseph Huntley <Joseph.Huntley@outlook.com>
2026-05-29 22:36:15 -0400

    Added get_db function

--- backend/app/db/session.py
+++ backend/app/db/session.py
@@ -4,5 +4,12 @@ from sqlalchemy.orm import sessionmaker
 DATABASE_URL = "postgresql+psycopg://postgres:password@localhost:5432/casedf"
 
 engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
 
-SessionLocal = sessionmaker(bind=engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
