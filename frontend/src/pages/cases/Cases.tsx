91077ab4155c21896e2521e36f2fa11f5eaa9a79
Joseph huntley <joseph.huntley@outlook.com>
2026-08-30 18:09:08 -0400

    Added cases header

--- frontend/src/pages/cases/Cases.tsx
+++ frontend/src/pages/cases/Cases.tsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import './Cases.css'
+import Button from '../../components/Button/Button'
+import { useState } from 'react'
 
 function Cases() {
+  const [activeFilter, setActiveFilter] = useState<string>('all')
+
+  
+
   return (
-    <div>Cases</div>
+    <main>
+      <div id='cases-header'>
+        <div id="cases-filter-btn">
+          <div className={`${activeFilter == 'all' ? 'cases-active' : ''}`} onClick={() => setActiveFilter('all')}>All</div>
+          <div className={`${activeFilter == 'active' ? 'cases-active' : ''}`} onClick={() => setActiveFilter('active')}>Active</div>
+          <div className={`${activeFilter == 'in_review' ? 'cases-active' : ''}`} onClick={() => setActiveFilter('in_review')}>In Review</div>
+          <div className={`${activeFilter == 'closed' ? 'cases-active' : ''}`} onClick={() => setActiveFilter('closed')}>Closed</div>
+        </div>
+        <Button id="cases-new-button" text="+ New Case"/>
+      </div>
+    </main>
   )
 }
 
